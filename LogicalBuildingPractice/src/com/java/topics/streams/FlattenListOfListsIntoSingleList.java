a8090c7611a9edfc690033c2d5f85888a25ad93c
Rishabh Kumar <srishabh759@gmail.com>
2024-08-12 12:21:32 +0530

    Added Question

--- LogicalBuildingPractice/src/com/java/topics/streams/FlattenListOfListsIntoSingleList.java
+++ LogicalBuildingPractice/src/com/java/topics/streams/FlattenListOfListsIntoSingleList.java
@@ -7,10 +7,12 @@ import java.util.stream.Collectors;
 public class FlattenListOfListsIntoSingleList {
     //Write a stream operation to flatten a list of lists into a single list.
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1, 2, 3);
-        List<Integer> list2 = Arrays.asList(4, 5, 6);
-        List<List<Integer>> list3 = Arrays.asList(list1, list2);
-//        List<Integer> list4 = list3.stream().collect(Collectors.flatMapping(s->s));
-        System.out.println(list3);
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7, 8, 9)
+        );
+        List<Integer> list = listOfLists.stream().flatMap(List::stream).toList();
+        System.out.println(list);
     }
 }
